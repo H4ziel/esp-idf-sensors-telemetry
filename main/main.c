3502e96e64f862c2d0ae157243bd8e09d0a08e04
Marcelo Haziel <marcelohaziel1@gmail.com>
2024-08-22 21:19:53 -0300

    LoRa Tx

--- main/main.c
+++ main/main.c
@@ -11,7 +11,7 @@
 #include <driver/uart.h>
 #include "hal/uart_types.h"
 #include "portmacro.h"
-//#include "lora.h"
+#include "lora.h"
 
 //#define configMAX_PRIORITIES 5 // nível máximo de prioridades ta definido como 25 no arquivo freertosconfig.h, então as prioridades são de 0(menor prioridade) a 24(maior prioridade), adicionar mais níveis de prioridade requer RAM
 
@@ -22,13 +22,15 @@
 #endif
 
 #define BUFFER 2024
+#define FREQUENCY 915e6
+#define BW 125e3
+
 #define TX_PIN 17
 #define RX_PIN 23
 
 static const char *TAG = "gy_87";  // TAG é utilizada nas funções ESP_LOG para referenciar tal função ou parte do código
 static const char *TAG1 = "GPS_NEO6m";
-
-const uart_port_t uart_numeration = UART_NUM_2; // indica que a UART 2 será utilizada, referente aos pinos GPIO01(tx) e GPIO03(rx) do esp
+static const char *TAG2 = "LoRa";
 
 typedef struct{
     uint32_t pressure_bmp;
@@ -49,21 +51,50 @@ typedef struct{
     float speed;
     float course; 
     char buf[BUFFER];
+    uint8_t packetLoRa[256];
 }variable;
+
 
 variable vars;
 void gy87(void*);
 void gps_init(void);
 void get_nmea(void*);
 void gps_neo6m(void*);
+esp_err_t setupLoRa(void);
+void sendLoRaData(void*);
 
 void app_main()
 {
     ESP_ERROR_CHECK(i2cdev_init());
+    ESP_ERROR_CHECK(setupLoRa());
 
-    xTaskCreatePinnedToCore(gy87, "gy87", configMINIMAL_STACK_SIZE * 6, (void*)&vars, configMAX_PRIORITIES - 1, NULL, 0);
-    xTaskCreatePinnedToCore(gps_neo6m, "gps_neo6m", configMINIMAL_STACK_SIZE * 6, (void*)&vars, configMAX_PRIORITIES - 1, NULL, 0);
-    xTaskCreatePinnedToCore(get_nmea, "get_nmea", configMINIMAL_STACK_SIZE * 6, (void*)&vars, configMAX_PRIORITIES - 1, NULL, 1);
+    xTaskCreatePinnedToCore(gy87, "gy87", configMINIMAL_STACK_SIZE + 2000, (void*)&vars, configMAX_PRIORITIES - 2, NULL, 0);
+    xTaskCreatePinnedToCore(gps_neo6m, "gps_neo6m", configMINIMAL_STACK_SIZE + 2000 , (void*)&vars, configMAX_PRIORITIES - 2, NULL, 0);
+    xTaskCreatePinnedToCore(get_nmea, "get_nmea", configMINIMAL_STACK_SIZE + 2000, (void*)&vars, configMAX_PRIORITIES - 2, NULL, 0);
+    xTaskCreatePinnedToCore(sendLoRaData, "Send_LoRa_Data", configMINIMAL_STACK_SIZE + 2000, (void*)&vars, configMAX_PRIORITIES - 1, NULL, 1);
+}
+
+esp_err_t setupLoRa(void){
+    
+    if(lora_init() == 0){
+        ESP_LOGE(TAG2, "Error!");
+        return ESP_FAIL;
+    }
+
+    lora_set_frequency(FREQUENCY);
+    lora_set_bandwidth(BW);
+    lora_set_spreading_factor(10);
+    //lora_set_tx_power(); lora__init() seta tx power em 17 dbm 
+    lora_enable_crc(); // CRC (verificação de redundancia ciclica) método de detecção de erros, que verifica a integridade dos dados transmitidos com os dados recebidos
+    lora_set_coding_rate(5);
+    lora_set_sync_word(0x12);
+    lora_set_preamble_length(6);
+
+    vTaskDelay(pdMS_TO_TICKS(500));
+
+    ESP_LOGW(TAG2, "LoRa OK!");
+
+    return ESP_OK;
 }
 
 void gy87(void *pvParameters)
@@ -92,8 +123,8 @@ void gy87(void *pvParameters)
 
     ESP_ERROR_CHECK(mpu6050_init(&dev));
 
-    ESP_LOGI(TAG, "Accel range: %d", dev.ranges.accel);
-    ESP_LOGI(TAG, "Gyro range:  %d", dev.ranges.gyro);
+    //ESP_LOGI(TAG, "Accel range: %d", dev.ranges.accel);
+    //ESP_LOGI(TAG, "Gyro range:  %d", dev.ranges.gyro);
 
     while (1)
     {
@@ -117,9 +148,12 @@ void gy87(void *pvParameters)
         ESP_LOGI(TAG, "**********************************************************************");
         ESP_LOGI(TAG, "Acceleration: x=%.4f   y=%.4f   z=%.4f", accel.x, accel.y, accel.z);
         ESP_LOGI(TAG, "Rotation:     x=%.4f   y=%.4f   z=%.4f", rotation.x, rotation.y, rotation.z);
-        ESP_LOGI(TAG, "Angles: Pitch=%.4f   Roll=%.4f", variables->anglePitchDeg, variables->angleRollDeg);
+        ESP_LOGI(TAG, "Angles: Pitch=%.1f   Roll=%.1f", variables->anglePitchDeg, variables->angleRollDeg);
         ESP_LOGI(TAG, "Temperature:  %.2f  Pressão:  %" PRIu32 " Pa", variables->temp, variables->pressure_bmp); // pra referenciar variavel do tipo uint32_t utiliza-se %" PRIu32 " da lib inttypes.h
 
+        UBaseType_t uxHighWaterMark = uxTaskGetStackHighWaterMark(NULL); // obtenção de espaço livre na task em words
+        ESP_LOGI(TAG,"Espaço mínimo livre na stack: %u\n", uxHighWaterMark);
+        
         vTaskDelay(pdMS_TO_TICKS(2000));
     }
 }
@@ -130,13 +164,14 @@ void gps_neo6m(void *pvParameters)
 
     gps_init(); // inicia o gps, evitar usar essa função mais de 1 vez.  
 
-    ESP_ERROR_CHECK(uart_set_pin(uart_numeration, TX_PIN, RX_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE)); // solução para nao ativar a GPIO_OLED_RST (mesmo pino q RX2) durante a iniciliazação da UART
-
     while(1){
         ESP_LOGI(TAG1, "Latitude: %s %.1s", variables->lat, variables->lat_dir);
         ESP_LOGI(TAG1, "Longitude: %.11s %.1s", variables->lon, variables->lon_dir);
         ESP_LOGI(TAG1, "Altitude: %.2f", variables->altitude);
         ESP_LOGI(TAG1, "Velocidade: %.3f", variables->speed);
+
+        UBaseType_t uxHighWaterMark = uxTaskGetStackHighWaterMark(NULL); // obtenção de espaço livre na task em words
+        ESP_LOGI(TAG1,"Espaço mínimo livre na stack: %u\n", uxHighWaterMark);
 
         vTaskDelay(pdMS_TO_TICKS(2000));
     }
@@ -144,6 +179,8 @@ void gps_neo6m(void *pvParameters)
 
 void gps_init(void)
 {
+    const uart_port_t uart_numeration = UART_NUM_2; // indica que a UART 2 será utilizada, referente aos pinos GPIO01(tx) e GPIO03(rx) do esp
+
     uart_config_t uart_configuration = {
         .baud_rate = 9600,
         .data_bits = UART_DATA_8_BITS,
@@ -156,7 +193,7 @@ void gps_init(void)
     ESP_ERROR_CHECK(uart_set_pin(uart_numeration, TX_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
     ESP_ERROR_CHECK(uart_driver_install(uart_numeration, BUFFER*2, 0, 0, NULL, 0));
 
-    vTaskDelay(pdMS_TO_TICKS(100));
+    vTaskDelay(pdMS_TO_TICKS(500));
 }
 
 void get_nmea(void *pvParameters){
@@ -170,6 +207,8 @@ void get_nmea(void *pvParameters){
     const char *GGA;  // identificador que possui latitude e longitude
     //const char *VTG; // identificador que possui velocidade em Km/h
     memset(variables->buf, 0, BUFFER);
+
+    ESP_ERROR_CHECK(uart_set_pin(UART_NUM_2, TX_PIN, RX_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE)); // solução para nao ativar a GPIO_OLED_RST (mesmo pino q RX2) durante a iniciliazação da UART
 
     while(1){
         uart_read_bytes(UART_NUM_2, variables->buf, BUFFER, pdMS_TO_TICKS(1000));
@@ -198,3 +237,62 @@ void get_nmea(void *pvParameters){
         snprintf(variables->lon, sizeof(variables->lon), "%c%c%c\xB0%c%c'%.3f\x22", variables->raw_lon[0], variables->raw_lon[1], variables->raw_lon[2], variables->raw_lon[3], variables->raw_lon[4], aux_lon_float * 60 / 100000);
     }   
 }
+
+void sendLoRaData(void *pvParameters){
+    variable *variables = (variable*)pvParameters;
+
+    char aux[50];
+
+    while(1){
+        strcpy((char *)variables->packetLoRa, "");
+        strcpy(aux, "");
+
+        sprintf(aux, "%.1f", variables->anglePitchDeg);
+        strcat((char *)variables->packetLoRa, aux);
+
+        sprintf(aux, "!%.1f", variables->angleRollDeg);
+        strcat((char *)variables->packetLoRa, aux);
+
+        sprintf(aux, "@%.2f", variables->temp);
+        strcat((char *)variables->packetLoRa, aux);
+
+        sprintf(aux, "#%lu", variables->pressure_bmp);
+        strcat((char *)variables->packetLoRa, aux);
+
+        sprintf(aux, "$%s", variables->lat);
+        strcat((char *)variables->packetLoRa, aux);
+
+        sprintf(aux, "¨%.1s", variables->lat_dir);
+        strcat((char *)variables->packetLoRa, aux);
+
+        sprintf(aux, "&%.11s", variables->lon);
+        strcat((char *)variables->packetLoRa, aux);
+
+        sprintf(aux, "*%.1s", variables->lon_dir);
+        strcat((char *)variables->packetLoRa, aux);
+
+        sprintf(aux, "(%.2f", variables->altitude);
+        strcat((char *)variables->packetLoRa, aux);
+        
+        sprintf(aux, ")%.3f£", variables->speed);
+        strcat((char *)variables->packetLoRa, aux);
+
+        for(int i = 0; i < sizeof(variables->packetLoRa); i++){
+            ESP_LOGI(TAG2, "Data [%s ]", (char*)&variables->packetLoRa[i]);
+            lora_send_packet(&variables->packetLoRa[i], 1);
+
+            if (strcmp((char *)variables->packetLoRa, "£") != 0){
+                ESP_LOGW(TAG2, "Packet sent!");
+                lora_end_packet(true);
+                i = sizeof(variables->packetLoRa);
+            }
+        }
+
+        UBaseType_t uxHighWaterMark = uxTaskGetStackHighWaterMark(NULL); // obtenção de espaço livre na task em words
+        ESP_LOGI(TAG2,"Espaço mínimo livre na stack: %u\n", uxHighWaterMark);
+        
+        vTaskDelay(pdMS_TO_TICKS(3000));
+    }
+    
+}
+
